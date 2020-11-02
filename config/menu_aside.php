132f3e8188b32ad96eb6917ca34ada91dd6a1ff0
unknown <arashia.sn@gmail.com>
2020-11-02 19:53:48 +0330

    Initial commit

---
+++ config/menu_aside.php
@@ -0,0 +1,82 @@
+<?php
+// Aside menu
+return [
+
+    'items' => [
+        // Dashboard
+        [
+            'title' => 'میزکار',
+            'root' => true,
+            'icon' => 'media/svg/icons/Design/Layers.svg', // or can be 'flaticon-home' or any flaticon-*
+            'page' => '/',
+            'new-tab' => false,
+        ],
+
+
+        // برنامه ها
+        [
+            'section' => 'برنامه ها',
+        ],
+        [
+            'title' => 'ثبت نام در برنامه ها',
+            'root' => true,
+            'icon' => 'media/svg/icons/Layout/Layout-4-blocks.svg', // or can be 'flaticon-home' or any flaticon-*
+            'page' => '',
+            'new-tab' => false,
+        ],
+        [
+            'title' => 'سوابق برنامه ها',
+            'root' => true,
+            'icon' => 'media/svg/icons/Shopping/Barcode-read.svg', // or can be 'flaticon-home' or any flaticon-*
+            'page' => '',
+            'new-tab' => false,
+        ],
+        [
+            'title' => 'نظرسنجی برنامه ها',
+            'root' => true,
+            'icon' => 'media/svg/icons/Shopping/Barcode-read.svg', // or can be 'flaticon-home' or any flaticon-*
+            'page' => '',
+            'new-tab' => false,
+        ],
+
+        // عضویت
+        [
+            'section' => 'پروفایل',
+        ],
+        [
+            'title' => 'مدارک آموزشی',
+            'root' => true,
+            'icon' => 'media/svg/icons/Shopping/Barcode-read.svg', // or can be 'flaticon-home' or any flaticon-*
+            'page' => '',
+            'new-tab' => false,
+        ],
+        [
+            'title' => 'سوابق و نوع عضویت',
+            'root' => true,
+            'icon' => 'media/svg/icons/Shopping/Barcode-read.svg', // or can be 'flaticon-home' or any flaticon-*
+            'page' => '',
+            'new-tab' => false,
+        ],
+        [
+            'title' => 'سوابق مالی',
+            'root' => true,
+            'icon' => 'media/svg/icons/Shopping/Barcode-read.svg', // or can be 'flaticon-home' or any flaticon-*
+            'page' => '',
+            'new-tab' => false,
+        ],
+        [
+            'title' => 'بیمه ورزشی',
+            'root' => true,
+            'icon' => 'media/svg/icons/Shopping/Barcode-read.svg', // or can be 'flaticon-home' or any flaticon-*
+            'page' => '',
+            'new-tab' => false,
+        ],
+
+
+        // CRUD
+
+
+    ]
+
+
+];
